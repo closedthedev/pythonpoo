47955a6aee07ea3a97b40cb5412b8388ae8ab74d
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-05-12 17:59:06 -0300

    att

--- projeto-minhasfinanças/main.py
+++ projeto-minhasfinanças/main.py
@@ -3,3 +3,25 @@ from utilitarios import (
     cadastrar_transacao,
     saldo_total,
 )
+
+#cadastrando categorias
+
+categoria_receita = cadastrar_categoria('Receitas')
+categoria_conta = cadastrar_categoria('Contas fixas')
+categoria_lazer = cadastrar_categoria('Lazer')
+categoria_viagem = cadastrar_categoria('Viagem')
+
+
+#cadastrando as transações
+
+cadastrar_transacao(descrição = 'Salário Jun/2024' , valor = 1700 , categoria =  categoria_receita) 
+cadastrar_transacao(descrição = 'Venda Celular' , valor = 1200 , categoria =  categoria_receita)
+cadastrar_transacao(descrição = 'Conta de luz' , valor = -550 , categoria =  categoria_conta)
+cadastrar_transacao(descrição = 'Viagem Arraial do Cabo' , valor = -300 , categoria =  categoria_viagem)
+cadastrar_transacao(descrição = 'Cinema' , valor = -150 , categoria =  categoria_lazer)
+
+total = saldo_total()
+
+print(f'Seu saldo final é de {total}')
+    
+    
