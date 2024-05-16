9ed21ee4511274390d4c2d3f3a58aa728d4671ef
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-05-15 22:45:23 -0300

    update and add new codes

--- projeto-banco/telainicial.py
+++ projeto-banco/telainicial.py
@@ -1,9 +1,34 @@
-print('''
-        ℂ𝕃𝕆𝕊𝔼𝔻 𝔹𝔸ℕ𝕂𝕀ℕ𝔾
-      ''')
+import os
+from utilitarios import Cliente, ContaBancaria, clientes_informacoes
 
-print('Olá, seja bem-vindo ao CLOSED BANKING. Para entrar em sua conta, digite seu CPF e o número da conta!\n')
+def tela_inicial():
+    print('''
+            ℂ𝕃𝕆𝕊𝔼𝔻 𝔹𝔸ℕ𝕂𝕀ℕ𝔾
+        ''')
 
+    print('Olá, seja bem-vindo ao CLOSED BANKING. Para entrar em sua conta, digite seu CPF e o número da conta!\n')
 
+def login_banco():
+        cpf = input('Digite o seu CPF: ').strip()
+        num_conta = input('Digite o número da sua conta bancária: ').strip()
 
+        cliente1, cliente2 = clientes_informacoes()
+        while True:
+            if cpf == '15323512394' and num_conta == '962937':
+                print(f'Olá, {cliente1.nome}. Seja bem-vindo!')
+                break
+            elif cpf == '17263518393' and num_conta == '178352':
+                print(f'Olá, {cliente2.nome}. Seja bem-vinda!')
+                break
+            else:
+                print("CPF ou número da conta inválido.")
+                cpf = input('Digite o seu CPF: ').strip()
+                num_conta = input('Digite o número da sua conta bancária: ').strip()
 
+def main():
+    os.system('cls')
+    tela_inicial()
+    login_banco()
+
+if __name__ == '__main__':
+    main()
---
+++ projeto-banco/utilitarios.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass
+
+@dataclass
+class Cliente:
+    nome: str
+    cpf: str
+    email: str
+@dataclass
+class ContaBancaria:
+    cliente: Cliente
+    numero_conta: str
+    saldo: int = 0
+
+    def exibir_nome_cliente(self):
+        return self.cliente.nome
+    
+    
+    def exibir_email_cliente(self):
+        return self.cliente.email
+    
+    def exibir_cpf_cliente(self):
+        return self.cliente.cpf
+    
+    def saldo_inicial(self):
+        while True:
+            try:
+                saldo_inicial = int(input('Digite seu saldo inicial: R$'))
+                if saldo_inicial > 0:
+                    self.saldo = saldo_inicial
+                    break
+            except ValueError:
+                 print("Erro: Por favor, insira um valor numérico válido.\n")
+
+    def depositar(self):
+        while True:
+            try:
+                valor = int(input('Quanto você quer depositar? R$'))
+                if valor > 0:
+                    self.saldo += valor
+                    print(f'Depósito concluído com sucesso. Novo saldo: R${self.saldo}')
+                    break  # Saia do loop somente se o depósito for bem-sucedido
+                else:
+                    print('Valor de depósito inválido! Por favor, insira um valor positivo.')
+            except ValueError:
+                 print("Erro: Por favor, insira um valor numérico válido.\n")
+
+    
+    def sacar(self):
+        while True:
+            try:
+                valor = int(input('Quanto você quer sacar? R$'))
+                if valor > 0 and valor <= self.saldo:
+                    self.saldo -= valor
+                    print(f'Saque realizado com sucesso. Novo saldo: R${self.saldo}')
+                    break
+                elif valor > self.saldo:
+                    print('Saldo insuficiente. Por favor, insira um valor menor.')
+                else:
+                    print('Valor incorreto! Por favor, insira um valor positivo.')
+            except ValueError:
+                print("Erro: Por favor, insira um valor numérico válido.")
+
+
+def clientes_informacoes():
+    cliente1 = Cliente(nome='Luiz Otavio', cpf='15323512394', email='closedthedev@gmail.com')
+    cliente2 = Cliente(nome='Rafaela', cpf='17263518393', email='rafaelathedev@gmail.com')
+
+    conta1 = ContaBancaria(cliente=cliente1, numero_conta='962937')
+    conta2 = ContaBancaria(cliente=cliente2, numero_conta='178352')
+
+    # Retorna as instâncias dos clientes
+    return cliente1, cliente2
+
+# Chamando a função para criar as instâncias
+cliente1, cliente2 = clientes_informacoes()
+
